ea8d478023a1a2678911a77827f85393bf17ada9
takayasugiyama <s.takaya1027@gmail.com>
2019-10-04 16:32:36 +0900

    [add] 不正な値のときのケースのmodel specを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  before_save {self.email.downcase}
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :name , presence: true, length: {in: 5..10}
+  validates :email, presence: true,format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,5 +7,19 @@ RSpec.describe User, type: :model do
         user.save 
         User.first.name = "test_user1"
       end
+
+      describe "名前は5文字以上10文字以下ではないと登録できない。"  do 
+         it "名前が4文字のときは登録できない" do  
+            user =  FactoryBot.build(:user, name: "あ" * 4)
+            user.valid? 
+            expect(user.errors[:name]).to include "は5文字以上で入力してください"
+         end
+
+         it "名前が11文字のときは登録できない"  do 
+            user = FactoryBot.build(:user,name: "あ" * 11)
+            user.valid?
+            expect(user.errors[:name]).to include "は10文字以内で入力してください"
+         end
+      end
    end
 end
