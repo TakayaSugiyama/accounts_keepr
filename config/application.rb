8251bfa679fa980b8f056d3a8f34ae98690e5512
takayasugiyama <s.takaya1027@gmail.com>
2019-10-04 14:18:02 +0900

    [add] 翻訳ファイルを追加

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,8 @@ Bundler.require(*Rails.groups)
 module AccoutsKeeper
   class Application < Rails::Application
     config.load_defaults 5.2
+
+    config.i18n.default_locale = :ja
     
     config.generators do |g| 
       g.test_framework :rspec, 
