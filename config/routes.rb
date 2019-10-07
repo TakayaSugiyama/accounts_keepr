7caa17401ed97e086bf53d44e55041809073341d
takayasugiyama <s.takaya1027@gmail.com>
2019-10-07 12:19:01 +0900

    [add] Users Controllerを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
+  resources :users, only: [:show]
   root to: "home#index"
 end
