1e090d6d5a7c887052c15ce85b92b85b9a7f2e26
takayasugiyama <s.takaya1027@gmail.com>
2019-10-07 12:38:34 +0900

    [add] マイページを追加、ログインしているときは、マイページへ飛ぶように変更

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,15 @@
 class HomeController < ApplicationController
-  def index
+  before_action :return_my_page
+
+  def index;end
+  
+  private
+  
+  #ログインしているときは、マイページへ飛ぶ
+  def return_my_page
+    if user_signed_in? 
+      redirect_to user_path(current_user.id)
+    end
   end
+
 end
