853e67d38fd70eef6e93b2944dcbadce48e48899
takayasugiyama <s.takaya1027@gmail.com>
2019-10-08 22:30:40 +0900

    [add] 名ページにグラフに追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    labels = @user.records.map(&:labels).map(&:first).map(&:name)
+    @chart = {}
+    labels.uniq.each do |name|
+      @chart["#{name}"] = labels.count(name)
+    end
+    @chart
   end
   
 end
