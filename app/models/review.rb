af280d43980f201ab8a0f8ae15573f6f694f3db1
takayasugiyama <s.takaya1027@gmail.com>
2019-10-11 10:30:50 +0900

    [fix] レビューのバリデーションを追加

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,5 +4,6 @@ class Review < ApplicationRecord
   has_many :images
   accepts_nested_attributes_for :images, reject_if: :all_blank, allow_destroy: true 
   validates :title, presence: true, length: {in: 5..20}
-  validates :content, length: {maximum: 200}
+  validates :content, length: {maximum: 385}
+  validates :rating, presence: true, length: {minimum: 1}
 end
