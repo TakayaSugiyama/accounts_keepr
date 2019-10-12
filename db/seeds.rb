fee1b05f3b850b66e3df45d9468fa37416e5ae1b
takayasugiyama <s.takaya1027@gmail.com>
2019-10-13 01:19:38 +0900

    [fix] seedファイルの編集

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,9 @@
-label_names = %w(未分類 食費 日用品費 レジャー費 交際費 車費 医療費 子ども費 美容費 被服費 趣味・娯楽費 交通費)
+label_names = %w(未分類 食費 日用品費 レジャー費 交際費 車費 医療費 養育費 美容費 被服費 趣味・娯楽費 交通費 家賃 水道・光熱費 保険 税・社会保証 自動車関連費  その他)
 
 label_names.each do |name|
   Label.create(name: name)
+end
+
+[*1..10].each do |i| 
+  User.create(name: "testuser#{i}",email: "sample#{i}@railsseed.org",password: "123456", password_confirmation: "123456")
 end
