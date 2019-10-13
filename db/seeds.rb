9735e0a362735375a49d4359e1e141281da606f9
takayasugiyama <s.takaya1027@gmail.com>
2019-10-13 15:40:13 +0900

    [fix] seedデータ編集

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,11 @@ label_names.each do |name|
   Label.create(name: name)
 end
 
-[*1..10].each do |i| 
-  User.create(name: "testuser#{i}",email: "sample#{i}@railsseed.org",password: "123456", password_confirmation: "123456")
+
+[*1..20].each do |i| 
+  user = User.create(name: "testuser#{i}",email: "sample#{i}@railsseed.org",password: "123456", password_confirmation: "123456")
+  record = Record.create(store_name: "store#{i}", user_id: i ,purchase_price: 100 * i , label_id: rand(Label.all.count) + 1, purchase_date: Random.rand(Date.new(Date.today.year, Date.today.month)..Date.new(Date.today.year,Date.today.month ,-1)) )
+  product = Product.create(name: "product#{i}", price: 100 * i, record_id: record.id )
+  Memo.create(content: "memoだお#{i}", record_id: record.id) 
+  Review.create(title: "title #{i}", content: "レビューだお#{i}", product_id: product.id , user_id: user.id , rating:  rand(5) + 1 ) 
 end
