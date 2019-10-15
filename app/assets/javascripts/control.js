ee74daae954465c7d1554908b0b19d9dd8a55c29
takayasugiyama <s.takaya1027@gmail.com>
2019-10-15 16:29:24 +0900

    [fix] メモの要素追加制限を追加

---
+++ app/assets/javascripts/control.js
@@ -0,0 +1,20 @@
+$(function() {
+  // limits the number of categories
+  $('#memos').on('cocoon:after-insert', function() {
+    check_to_hide_or_show_memo_link();
+  });
+
+  $('#memos').on('cocoon:after-remove', function() {
+    check_to_hide_or_show_memo_link();
+  });
+
+ 
+
+  function check_to_hide_or_show_memo_link() {
+    if ($('#memos .nested-fields').length == 2) {
+      $('#add-memo').hide();
+    } else {
+      $('#add-memo').show();
+    }
+  }
+})
