b9ff2a4fd33ff214f83491f7448a0861d79dfd07
takayasugiyama <s.takaya1027@gmail.com>
2019-10-21 12:55:25 +0900

    [fix] 設定ファイルもやり直す

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,4 +1,8 @@
 CarrierWave.configure do |config|
+  require 'carrierwave/storage/abstract'
+  require 'carrierwave/storage/file'
+  require 'carrierwave/storage/fog'
+
   config.storage :fog
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
@@ -6,10 +10,15 @@ CarrierWave.configure do |config|
       aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
       aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
       region:                'ap-northeast-1',
+      path_style:            true,
   }
+
+  config.fog_public     = true
+  config.fog_attributes = {'Cache-Control' => 'public, max-age=86400'}
 
   case Rails.env
     when 'production'
       config.fog_directory = 'accountskeeper'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/accountskeeper'
   end
 end
