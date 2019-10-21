f53a8559b00426fa2fa1d54b00d3601f5a6ce544
takayasugiyama <s.takaya1027@gmail.com>
2019-10-21 12:10:53 +0900

    [fix] 余計な記述を消してみる

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,4 @@
 CarrierWave.configure do |config|
-  require 'carrierwave/storage/abstract'
-  require 'carrierwave/storage/file'
-  require 'carrierwave/storage/fog'
-
   config.storage :fog
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
@@ -10,7 +6,6 @@ CarrierWave.configure do |config|
       aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
       aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
       region:                'ap-northeast-1',
-      path_style:            true,
   }
 
   config.fog_public     = true
@@ -19,6 +14,5 @@ CarrierWave.configure do |config|
   case Rails.env
     when 'production'
       config.fog_directory = 'accountskeeper-production'
-      config.asset_host = 'https://accountskeeper-production.s3-ap-northeast-1.amazonaws.com'
   end
 end
