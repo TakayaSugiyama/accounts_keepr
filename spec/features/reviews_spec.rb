4b68be39a803dfbb6049eb5b7ca72364400f5c16
takayasugiyama <s.takaya1027@gmail.com>
2019-10-23 13:43:08 +0900

    [add] レビューの更新/削除のテストを追加

--- spec/features/reviews_spec.rb
+++ spec/features/reviews_spec.rb
@@ -22,5 +22,23 @@ RSpec.feature "Review", type: :feature do
       expect(Review.all.count).to eq 1
     end
 
+    it "レビューを削除できる" do 
+      review = FactoryBot.create(:review,user_id: @user.id, product_id: @product.id)
+      expect(Review.all.count).to eq 1
+      visit review_path(review)
+      click_on "削除"
+      expect(Review.all.count).to eq 0
+    end
+
+    it "レビューを更新できる" do 
+      review = FactoryBot.create(:review,user_id: @user.id, product_id: @product.id) 
+      visit review_path(review)
+      click_on "編集"
+      fill_in "タイトル", with: "updated"
+      fill_in "レビュー", with: "updated"
+      find('#review_rating', visible: false).set(5)
+      click_on "更新する"
+      expect(page).to  have_content "レビューを更新しました"
+    end
   end
 end
