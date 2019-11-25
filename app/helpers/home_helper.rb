e20009c163154aab661a99f575e3ac9a949a765d
takayasugiyama <s.takaya1027@gmail.com>
2019-11-25 23:57:05 +0900

    タイトルをページごとに変える

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -2,10 +2,7 @@
 
 module HomeHelper
   def header_menu_path
-    if user_signed_in?
-      'shared/layouts/login_user'
-    else
-      'shared/layouts/not_login_user'
-    end
+    user_signed_in? ? 'shared/layouts/login_user': 'shared/layouts/not_login_user'
   end
+  
 end
