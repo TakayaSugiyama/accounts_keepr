479f8d71c61b90f6951d23243c357690161cb2f4
takayasugiyama <s.takaya1027@gmail.com>
2019-11-28 18:30:28 +0900

    家計簿を編集時と作成時の日付のずれを修正

--- app/helpers/records_helper.rb
+++ app/helpers/records_helper.rb
@@ -4,4 +4,12 @@ module RecordsHelper
   def edit_or_new(record)
     record.label.id if action_name == 'edit'
   end
+
+  def form_date(record)
+     if action_name == "new"
+        Date.today 
+     elsif action_name == "edit"
+        @record.purchase_date
+     end
+  end
 end
