409264fa281fccc1582e8adef03ba7c01d5a1cdc
takayasugiyama <s.takaya1027@gmail.com>
2020-09-14 01:07:56 +0900

    rubocop 修正

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -12,9 +12,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     user = User.from_omniauth(request.env['omniauth.auth'])
     if user.persisted?
       sign_in_and_redirect user, event: :authentication
-      if is_navigational_format?
-        set_flash_message(:notice, :success, kind: provider.to_s.capitalize)
-      end
+      set_flash_message(:notice, :success, kind: provider.to_s.capitalize) if is_navigational_format?
     else
       session["devise.#{provider}_data"] = request.env['omniauth.auth'].except('extra')
       redirect_to new_user_registration_url, notice: '登録できませんでした'
@@ -23,7 +21,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def after_sign_in_path_for(_resource)
     session[:previous_url].nil? ? current_user : session[:previous_url]
-   end
+  end
 
   def failure
     redirect_to root_path
