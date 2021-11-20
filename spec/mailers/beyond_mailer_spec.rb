82380bbf9a4769121d542c6a1daf26a75b57ee4c
takayasugiyama <s.takaya1027@gmail.com>
2021-11-21 06:01:27 +0900

    :+1: add beyond_mailer_spec

--- spec/mailers/beyond_mailer_spec.rb
+++ spec/mailers/beyond_mailer_spec.rb
@@ -1,16 +1,17 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
-describe AlertMailer, type: :mailer do
+
+describe BeyondMailer, type: :mailer do
   context '.notify' do
     let(:user) { FactoryBot.create(:user, email: 's.takaya1027@example.com') }
     let(:estimate_amount) { FactoryBot.create(:estimate_amount) }
     let(:sum_price) { 100 }
 
-    let(:email) { AlertMailer.alert_mail(user, estimate_amount, sum_price) }
+    let(:email) { BeyondMailer.beyond_mail(user, estimate_amount, sum_price) }
 
     it 'renders the headers' do
-      expect(email.subject).to eq '今月の設定金額が近づいています。'
+      expect(email.subject).to eq '今月の設定金額を超えました。'
       expect(email.to).to eq %w[s.takaya1027@example.com]
       expect(email.from).to eq %w[from@example.com]
     end
